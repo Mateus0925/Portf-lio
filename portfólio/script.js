ee963eaa6dd6890c45630ce62c656071ce074c2c
Mateus Fernandes <josemateus0925@gmail.com>
2025-03-09 19:27:31 -0300

    bloco-5 terminado

---
+++ portfólio/script.js
@@ -0,0 +1,25 @@
+function copiarTexto(elemento) {
+    const texto = elemento.textContent || elemento.innerText;
+    // Cria um elemento temporário de input para copiar o texto
+    const tempInput = document.createElement('input');
+    document.body.appendChild(tempInput);
+    tempInput.value = texto;  // Define o valor do input para o texto a ser copiado
+    tempInput.select();  // Seleciona o texto
+    document.execCommand('copy');
+    // Remove o input temporário
+    document.body.removeChild(tempInput);
+    elemento.classList.add('copiado');
+    elemento.textContent = 'Texto copiado!';
+    setTimeout(() => {
+        elemento.textContent = texto;
+        elemento.classList.remove('copiado');
+    }, 1500);
+}
+
+// Adiciona o ouvinte de evento de clique a cada elemento 
+const textosParaCopiar = document.querySelectorAll('.textoParaCopiar');
+textosParaCopiar.forEach(function (texto) {
+    texto.addEventListener('click', function () {
+        copiarTexto(texto);
+    });
+});
